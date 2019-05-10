8a9e7f20a831d0d9bb97e8862e275837febe1a9b
William Hendricks <wshendricks@gmail.com>
2019-05-10 02:30:01 -0500

    Final commit before adding in Mongoose.

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/fruit', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
 /* GET Hello World page. */
 router.get('/helloworld', function(req,res) {
   res.render(('helloworld'), { title: 'Hello, World! '});
